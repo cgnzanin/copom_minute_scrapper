edfb942897e97f18dc24cba6f2a40d3fc994b907
Zanin <cleber.zanin@usp.br>
2024-06-26 22:02:34 -0300

    primeiro commit

---
+++ README.md
@@ -0,0 +1,60 @@
+
+# COPOM Minutes Scraper
+
+Este projeto é uma aplicação de web scraping em Python para extrair e processar as atas das reuniões do COPOM (Comitê de Política Monetária do Banco Central do Brasil).
+
+## Funcionalidades
+
+- Extrai dados de atas de reuniões antigas e novas do COPOM.
+- Processa tanto conteúdos HTML quanto PDFs.
+- Salva os textos extraídos em um arquivo de saída.
+
+## Estrutura do Projeto
+
+- `fetch_json(url: str) -> dict`: Faz uma requisição GET para a URL fornecida e retorna o JSON.
+- `extract_data(json_data: dict, link_key: str) -> pd.DataFrame`: Normaliza o conteúdo do JSON e extrai colunas específicas.
+- `get_copom_data() -> pd.DataFrame`: Combina dados de APIs de atas antigas e novas em um único DataFrame.
+- `fetch_html_content(link: str, base_url: str = "https://www.bcb.gov.br") -> str`: Extrai e processa o conteúdo HTML das atas antigas.
+- `fetch_pdf_content(link: str, base_url: str = "https://www.bcb.gov.br") -> str`: Faz uma requisição GET para a URL do "PDF" e processa o conteúdo.
+- `fetch_content(row)`: Determina o tipo de conteúdo (PDF ou HTML) e chama a função apropriada para extrair o texto.
+- `process_atas() -> pd.DataFrame`: Processa as atas e extrai o conteúdo completo para cada uma.
+- `parcial(text: str) -> str`: Extrai partes do texto para visualização.
+- `main()`: Executa o processo completo e salva o resultado em um arquivo de saída.
+
+## Requisitos
+
+- Python 3.8+
+- Bibliotecas Python listadas no arquivo `requirements.txt`
+
+## Como Usar
+
+1. Clone o repositório:
+    ```bash
+    git clone https://github.com/seu-usuario/copom-minutes-scraper.git
+    cd copom-minutes-scraper
+    ```
+
+2. Crie um ambiente virtual e ative-o:
+    ```bash
+    python -m venv venv
+    source venv/bin/activate  # Linux/Mac
+    venv\Scriptsctivate  # Windows
+    ```
+
+3. Instale as dependências:
+    ```bash
+    pip install -r requirements.txt
+    ```
+
+4. Execute o script:
+    ```bash
+    python main.py
+    ```
+
+## Saída
+
+O script gera um arquivo `df_atas.fst` na pasta `output`, contendo as atas das reuniões do COPOM processadas.
+
+## Licença
+
+Este projeto está licenciado sob a MIT License.
---
+++ main.py
@@ -0,0 +1,112 @@
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+import fitz  # PyMuPDF
+from typing import Dict
+
+
+def fetch_json(url: str) -> Dict:
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.json()
+
+
+def extract_data(json_data: Dict, link_key: str) -> pd.DataFrame:
+    content = json_data["conteudo"]
+    df = pd.json_normalize(content)
+    df = df[["DataReferencia", "Titulo", link_key]]
+    df["Tipo"] = "pdf" if link_key == "Url" else "html"
+    df.rename(columns={link_key: "LinkPagina"}, inplace=True)
+    return df
+
+
+def get_copom_data() -> pd.DataFrame:
+    base_url = "https://www.bcb.gov.br"
+    api_antigas = (
+        "/api/servico/sitebcb/atascopom-conteudo/ultimas?quantidade=1000&filtro="
+    )
+    api_novas = "/api/servico/sitebcb/atascopom/ultimas?quantidade=1000&filtro="
+
+    json_antigas = fetch_json(f"{base_url}{api_antigas}")
+    json_novas = fetch_json(f"{base_url}{api_novas}")
+
+    df_antigas = extract_data(json_antigas, "LinkPagina")
+    df_novas = extract_data(json_novas, "Url")
+
+    df_atas = pd.concat([df_antigas, df_novas], ignore_index=True)
+    return df_atas
+
+
+def fetch_html_content(link: str, base_url: str = "https://www.bcb.gov.br") -> str:
+    api = "/api/servico/sitebcb/atascopom-conteudo/principal?filtro=IdentificadorUrl"
+    code = link.split("/")[-1]
+    encoded_code = f" eq '{code}'"
+    json_url = f"{base_url}{api}{encoded_code}"
+    json_response = fetch_json(json_url)
+
+    # Inspecionando a estrutura do JSON
+    print(json_response.keys())
+    if "conteudo" in json_response:
+        if (
+            isinstance(json_response["conteudo"], list)
+            and len(json_response["conteudo"]) > 0
+        ):
+            first_item = json_response["conteudo"][0]
+            print(first_item.keys())
+            if "OutrasInformacoes" in first_item:
+                html_content = first_item["OutrasInformacoes"]
+                soup = BeautifulSoup(html_content, "html.parser")
+                text = soup.get_text(separator=" ", strip=True)
+                return text
+            else:
+                raise KeyError(
+                    "Key 'OutrasInformacoes' not found in the first item of 'conteudo'."
+                )
+        else:
+            raise TypeError("'conteudo' is not a list or is empty.")
+    else:
+        raise KeyError("Key 'conteudo' not found in the JSON response.")
+
+
+def fetch_pdf_content(link: str, base_url: str = "https://www.bcb.gov.br") -> str:
+    pdf_url = f"{base_url}{link}"
+    response = requests.get(pdf_url)
+    response.raise_for_status()
+    # Verificando se o conteúdo é realmente um PDF
+    if response.headers["Content-Type"] == "application/pdf":
+        pdf_document = fitz.open(stream=response.content, filetype="pdf")
+        text = ""
+        for page in pdf_document:
+            text += page.get_text()
+        return text
+    else:
+        # Processando como HTML caso não seja um PDF
+        soup = BeautifulSoup(response.content, "html.parser")
+        text = soup.get_text(separator=" ", strip=True)
+        return text
+
+
+def fetch_content(row):
+    if row["Tipo"] == "pdf":
+        return fetch_pdf_content(row["LinkPagina"])
+    return fetch_html_content(row["LinkPagina"])
+
+
+def process_atas() -> pd.DataFrame:
+    df_atas = get_copom_data()
+    df_atas["integra"] = df_atas.apply(fetch_content, axis=1)
+    return df_atas
+
+
+def parcial(text: str) -> str:
+    return f"{text[:500]} (.......) {text[-500:]}"
+
+
+def main():
+    df_atas = process_atas()
+    df_atas["parcial"] = df_atas["integra"].apply(parcial)
+    df_atas.to_fst("assets/df_atas.fst", compress=50)
+
+
+if __name__ == "__main__":
+    main()
